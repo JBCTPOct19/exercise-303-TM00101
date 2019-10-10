b64e5e2a8ecffdb2b7c7657c5c0a08470410ffa3
TPCF235016UR <TPCF235016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-10 16:09:29 -0400

    Ex303

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String taskName;
+
+    @NotNull
+    private String dueDate;
+
+    @NotNull
+    private int priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
